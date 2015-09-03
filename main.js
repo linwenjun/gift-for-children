2f8800517d10c01118e26401ed0492c61009e2d3
Wenjun LIN <wenjun.lin@rea-group.com>
2015-09-03 23:01:20 +0800

    change wording for download

--- main.js
+++ main.js
@@ -15,17 +15,13 @@ for (var i = 1; i <= 10; i++) {
 }
 
 $(function() {
-  // $('#player').get(0).play();
 
   data.forEach(function(item, idx) {
-    // $('<div/>').addClass('media-button').html(item.title).data('src', item.path).appendTo(container);
-    // $('<a/>').html('下载').attr('src', item.path).appendTo(container);
-    // container.appendTo('#media-list');
     var tr = $('<tr/>');
     $('<td>').html(idx + 1).appendTo(tr);
     $('<td>').html(item.title).appendTo(tr);
     $('<td>').html('<a class="play" data-src="'+item.path+'">播放</a>').appendTo(tr);
-    $('<td>').html('<a href="'+item.path+'">右键下载</a>').appendTo(tr);
+    $('<td>').html('<a href="'+item.path+'">下载</a>').appendTo(tr);
     // tr.append('<td>').html(item.title);
     // tr.append('<td>').html('<a>');
     tr.appendTo('#media-list');
